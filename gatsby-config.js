88b9bff800fcfd99ac25f9411019ea7b2af12954
Elijah Shepard <elijah@eccentricpixel.com>
2021-11-13 15:38:56 -0800

    ...

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,8 +34,7 @@ module.exports = {
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
-        display: `minimal-ui`,
-        quality: 75,
+        display: `minimal-ui`,        
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
